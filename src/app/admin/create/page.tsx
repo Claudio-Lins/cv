496ab2df14c49bd02f03ed7d40f7df0363905ddb
Claudio Lins <claudio.lins@me.com>
2024-09-17 00:00:39 +0100

    feat: Biome

--- src/app/admin/create/page.tsx
+++ src/app/admin/create/page.tsx
@@ -1,37 +1,43 @@
-import { CreateResumeForm } from "@/components/forms/create-resume-form"
-import { getEducation } from "@/data/educations"
-import { getReferences } from "@/data/references"
-import { getSkills } from "@/data/skills"
-import { getSocialNetworks } from "@/data/social-network"
-import { getWorkExperiences } from "@/data/work-experiences"
-import { cn } from "@/lib/utils"
-import type { ResumeTypes } from "../../../../@types/resume-types"
+import { CreateResumeForm } from '@/components/forms/create-resume-form'
+import { getEducation } from '@/data/educations'
+import { getReferences } from '@/data/references'
+import { getSkills } from '@/data/skills'
+import { getSocialNetworks } from '@/data/social-network'
+import { getWorkExperiences } from '@/data/work-experiences'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import type { ResumeTypes } from '../../../../@types/resume-types'
 
 interface CreateResumeProps {}
 
 export default async function CreateResume({}: CreateResumeProps) {
-  try {
-    const [skills, socialNetworks, workExperiences, educations, references] =
-      await Promise.all([
-        getSkills(),
-        getSocialNetworks(),
-        getWorkExperiences(),
-        getEducation(),
-        getReferences(),
-      ])
-    return (
-      <div className={cn("w-full max-w-7xl mt-20 mx-auto bg-white")}>
-        <CreateResumeForm
-          skills={skills}
-          socialNetworks={socialNetworks}
-          workExperiences={workExperiences}
-          educations={educations}
-          references={references}
-        />
-      </div>
-    )
-  } catch (error) {
-    console.error("Erro ao carregar dados:", error)
-    return <div>Erro ao carregar dados</div>
-  }
+	try {
+		const [skills, socialNetworks, workExperiences, educations, references] = await Promise.all([
+			getSkills(),
+			getSocialNetworks(),
+			getWorkExperiences(),
+			getEducation(),
+			getReferences(),
+		])
+		return (
+			<div className={cn('w-full max-w-7xl mt-20 mx-auto bg-white')}>
+				<CreateResumeForm
+					skills={skills}
+					socialNetworks={socialNetworks}
+					workExperiences={workExperiences}
+					educations={educations}
+					references={references}
+				/>
+			</div>
+		)
+	} catch (error) {
+		console.error('Erro ao carregar dados:', error)
+		return (
+			<div className='w-full h-dvh flex items-center justify-center'>
+				<Link href={'/'} className='flex items-center gap-2 border rounded-md bg-black text-white px-2 py-1'>
+					<span>Voltar para a página inicial</span>
+				</Link>
+			</div>
+		)
+	}
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,60 @@
-import { ResumeCard } from "@/components/resume-card"
-import { getEducation } from "@/data/educations"
-import { getReferences } from "@/data/references"
-import { getSkills } from "@/data/skills"
-import { getSocialNetworks } from "@/data/social-network"
-import { getWorkExperiences } from "@/data/work-experiences"
-import { getResumeData, getResumes } from "@/services/resumeService"
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { Plus } from "lucide-react"
-import Link from "next/link"
+import { ResumeCard } from '@/components/resume-card'
+import { getEducation } from '@/data/educations'
+import { getReferences } from '@/data/references'
+import { getSkills } from '@/data/skills'
+import { getSocialNetworks } from '@/data/social-network'
+import { getWorkExperiences } from '@/data/work-experiences'
+import { getResumeData, getResumes } from '@/services/resumeService'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { Plus } from 'lucide-react'
+import Link from 'next/link'
 
 interface ResumeProps {
-  params: {
-    slug: string
-  }
+	params: {
+		slug: string
+	}
 }
 
 export default async function Resume({ params }: ResumeProps) {
-  const { getUser } = getKindeServerSession()
-  const user = await getUser()
-  const { resumes } = await getResumeData(user?.id!, params.slug)
-  const allSocialNetworks = await getSocialNetworks()
-  const allWorkExperiences = await getWorkExperiences()
-  const allSkills = await getSkills()
-  const allEducations = await getEducation()
-  const allReferences = await getReferences()
+	const { getUser } = getKindeServerSession()
+	const user = await getUser()
+	const { resumes } = await getResumeData(user?.id!, params.slug)
+	const allSocialNetworks = await getSocialNetworks()
+	const allWorkExperiences = await getWorkExperiences()
+	const allSkills = await getSkills()
+	const allEducations = await getEducation()
+	const allReferences = await getReferences()
 
-  return (
-    <div className="w-full p-20">
-      <div className="flex flex-wrap justify-center gap-10">
-        <Link
-          href="/admin/create/"
-          className="rounded-lg  w-96 h-64 overflow-hidden flex flex-col hover:scale-105 transition-all duration-500 hover:shadow-lg border-dashed border border-zinc-800 cursor-pointer group"
-        >
-          <div className="p-4 flex-grow flex items-center justify-center">
-            <Plus
-              size={100}
-              className="text-zinc-400 group-hover:text-zinc-600 transition-all duration-500 group-hover:rotate-180"
-            />
-          </div>
-        </Link>
-        {resumes.map((resume) => (
-          <ResumeCard
-            id={resume.id}
-            resume={resume}
-            slug={resume.slug}
-            key={resume.id}
-            title={resume.title}
-            about={resume?.about}
-            allSocialNetworks={allSocialNetworks}
-            allWorkExperiences={allWorkExperiences}
-            allSkills={allSkills}
-            allEducations={allEducations}
-            allReferences={allReferences}
-          />
-        ))}
-      </div>
-    </div>
-  )
+	return (
+		<div className='w-full p-20'>
+			<div className='flex flex-wrap justify-center gap-10'>
+				<Link
+					href='/admin/create/'
+					className='rounded-lg  w-96 h-64 overflow-hidden flex flex-col hover:scale-105 transition-all duration-500 hover:shadow-lg border-dashed border border-zinc-800 cursor-pointer group'
+				>
+					<div className='p-4 flex-grow flex items-center justify-center'>
+						<Plus
+							size={100}
+							className='text-zinc-400 group-hover:text-zinc-600 transition-all duration-500 group-hover:rotate-180'
+						/>
+					</div>
+				</Link>
+				{resumes.map((resume) => (
+					<ResumeCard
+						id={resume.id}
+						resume={resume}
+						slug={resume.slug}
+						key={resume.id}
+						title={resume.title}
+						about={resume?.about}
+						allSocialNetworks={allSocialNetworks}
+						allWorkExperiences={allWorkExperiences}
+						allSkills={allSkills}
+						allEducations={allEducations}
+						allReferences={allReferences}
+					/>
+				))}
+			</div>
+		</div>
+	)
 }
