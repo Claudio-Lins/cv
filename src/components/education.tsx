4fb0534f6aa866734d42961d104beafa449159b5
Claudio Lins <claudio.lins@me.com>
2024-09-18 17:05:29 +0100

    fix: Print

--- src/components/education.tsx
+++ src/components/education.tsx
@@ -1,62 +1,55 @@
-import { cn } from "@/lib/utils"
-import dayjs from "dayjs"
-import duration from "dayjs/plugin/duration"
-import relativeTime from "dayjs/plugin/relativeTime"
-import { calculateDuration } from "@/utils/caculate-duration-data"
+import { cn } from '@/lib/utils'
+import { calculateDuration } from '@/utils/caculate-duration-data'
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 dayjs.extend(duration)
 
 interface EducationProps {
-  educations: {
-    id: string
-    school: string
-    field: string
-    startDate: Date
-    endDate: Date | null
-  }[]
+	educations: {
+		id: string
+		school: string
+		field: string
+		startDate: Date
+		endDate: Date | null
+	}[]
 }
 
 export function Education({ educations }: EducationProps) {
-  return (
-    <div
-      className={cn(
-        "font-light text-zinc-600 tracking-widest print:tracking-wider"
-      )}
-    >
-      <h3 className=" uppercase font-light myPrintingSubTitle">Education</h3>
-      <div className="flex flex-col mt-6 space-y-4 print:space-y-4">
-        {educations?.map((edu) => (
-          <div className="flex flex-col space-y-1 print:space-y-0" key={edu.id}>
-            <h4 className=" uppercase  text-zinc-600 text-sm font-semibold print:text-[10px] print:leading-snug">
-              {edu?.field}
-            </h4>
-            <small className="print:text-[8px] text-xs">{edu?.school}</small>
-            <div className="flex items-center justify-start gap-2">
-              <small className="print:text-[8px] text-xs">
-                {new Intl.DateTimeFormat("pt-PT", {
-                  year: "numeric",
-                  month: "short",
-                }).format(edu.startDate)}
-              </small>
-              <span className="print:text-[8px] text-xs">|</span>
-              <small className="print:text-[8px] text-xs">
-                {edu.endDate
-                  ? new Intl.DateTimeFormat("pt-PT", {
-                      year: "numeric",
-                      month: "short",
-                    }).format(edu.endDate)
-                  : "N/A"}
-              </small>
-              <small className="text-gray-400 print:hidden text-xs">
-                {calculateDuration(
-                  edu.startDate.toString(),
-                  edu?.endDate?.toString()
-                )}
-              </small>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+	return (
+		<div className={cn('font-light text-zinc-600 tracking-widest print:tracking-wider')}>
+			<h3 className=' uppercase font-light myPrintingSubTitle'>Education</h3>
+			<div className='flex flex-col mt-6 space-y-4 print:space-y-4'>
+				{educations?.map((edu) => (
+					<div className='flex flex-col space-y-1 print:space-y-0 print:pr-4' key={edu.id}>
+						<h4 className=' uppercase  text-zinc-600 text-sm font-semibold print:text-[10px] print:font-medium print:leading-snug'>
+							{edu?.field}
+						</h4>
+						<small className='print:text-[8px] text-xs'>{edu?.school}</small>
+						<div className='flex items-center justify-start gap-2'>
+							<small className='print:text-[8px] text-xs'>
+								{new Intl.DateTimeFormat('pt-PT', {
+									year: 'numeric',
+									month: 'short',
+								}).format(edu.startDate)}
+							</small>
+							<span className='print:text-[8px] text-xs'>|</span>
+							<small className='print:text-[8px] text-xs'>
+								{edu.endDate
+									? new Intl.DateTimeFormat('pt-PT', {
+											year: 'numeric',
+											month: 'short',
+										}).format(edu.endDate)
+									: 'N/A'}
+							</small>
+							<small className='text-gray-400 print:hidden text-xs'>
+								{calculateDuration(edu.startDate.toString(), edu?.endDate?.toString())}
+							</small>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
--- src/components/references.tsx
+++ src/components/references.tsx
@@ -1,40 +1,36 @@
-import { cn } from "@/lib/utils"
-import { calculateDuration } from "@/utils/caculate-duration-data"
-import dayjs from "dayjs"
-import duration from "dayjs/plugin/duration"
-import relativeTime from "dayjs/plugin/relativeTime"
-import { Globe } from "lucide-react"
-import { ReferenceTypes } from "../../@types/resume-types"
+import { cn } from '@/lib/utils'
+import { calculateDuration } from '@/utils/caculate-duration-data'
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import { Globe } from 'lucide-react'
+import { ReferenceTypes } from '../../@types/resume-types'
 
 dayjs.extend(duration)
 
 interface ReferencesProps {
-  references: ReferenceTypes[]
+	references: ReferenceTypes[]
 }
 
 export function References({ references }: ReferencesProps) {
-  return (
-    <div className={cn("font-light text-zinc-600 tracking-widest ")}>
-      <h3 className=" uppercase font-light myPrintingSubTitle">References</h3>
-      <div className=" flex flex-wrap gap-4">
-        {references?.map((reference) => {
-          return (
-            <div
-              key={reference.id}
-              className="flex flex-col space-y-1 w-full max-w-[300px] mt-6 relative p-4 border border-dashed border-zinc-300 print:max-w-[250px]"
-            >
-              <h3 className="font-bold uppercase print:text-[10px]">
-                {reference?.name}
-              </h3>
-              <p className="font-semibold text-xs print:text-[10px]">
-                {reference?.role}
-              </p>
-              <p className="text-xs print:text-[10px]">{reference?.phone}</p>
-              <p className="text-xs print:text-[10px]">{reference?.email}</p>
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  )
+	return (
+		<div className={cn('font-light text-zinc-600 tracking-widest ')}>
+			<h3 className=' uppercase font-light myPrintingSubTitle'>References</h3>
+			<div className=' flex flex-col gap-4'>
+				{references?.map((reference) => {
+					return (
+						<div
+							key={reference.id}
+							className='flex flex-col space-y-1 w-full max-w-[300px] mt-6 relative print:max-w-[250px]'
+						>
+							<h3 className='font-bold uppercase print:text-[10px]'>{reference?.name}</h3>
+							<p className='font-semibold text-xs print:text-[10px]'>{reference?.role}</p>
+							<p className='text-xs print:text-[10px]'>{reference?.phone}</p>
+							<p className='text-xs print:text-[10px]'>{reference?.email}</p>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
